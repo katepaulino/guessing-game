c864a774af1ce3294c02026a566850079cee806d
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-01-17 10:12:12 -0800

    wrapped script in onload so fonts and images render before prompts execute

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-//Counter for Kate's Guessing Game.
-var numCorrect = 0;
+window.onload = function() {
+  var numCorrect = 0;
 
-var ans1 = document.getElementById('a1');
-var ans2 = document.getElementById('a2');
-var ans3 = document.getElementById('a3');
-var correctCount = document.getElementById('elCorrect');
+  var ans1 = document.getElementById('a1');
+  var ans2 = document.getElementById('a2');
+  var ans3 = document.getElementById('a3');
+  var correctCount = document.getElementById('elCorrect');
 
-var questions = ['Can you guess what country I was born in?', 'Another fact about me is that I love sharks. Can you guess which shark species live in the Puget Sound? Choose one: great white shark, mako shark, sixgill shark or the black tip reef shark.', 'Can you guess how many siblings I have?'];
-var answers = ['PHILIPPINES', 'SIXGILL SHARK', 2];
-var els = [ans1, ans2, ans3];
+  var questions = ['Can you guess what country I was born in?', 'Another fact about me is that I love sharks. Can you guess which shark species live in the Puget Sound? Choose one: great white shark, mako shark, sixgill shark or the black tip reef shark.', 'Can you guess how many siblings I have?'];
+  var answers = ['PHILIPPINES', 'SIXGILL SHARK', 2];
+  var els = [ans1, ans2, ans3];
 
-//Game function should take from questions array compares user answers to answers array and posts to html
-function game(questions, answers, index) {
+  //Game function should take from questions array compares user answers to answers array and posts to html
+  function game(questions, answers, index) {
   var queary = prompt(questions).toUpperCase();
 
   if (queary === answers || queary === parseInt(answers)) {
@@ -24,8 +24,9 @@ function game(questions, answers, index) {
   }
 }
 
-for (var i = 0; i < questions.length; i++) {
-  window.setTimeout(game(questions[i], answers[i], i), 1000);
-}
+  for (var i = 0; i < questions.length; i++) {
+    window.setTimeout(game(questions[i], answers[i], i), 1000);
+  }
 
-elCorrect.textContent = 'You got ' + numCorrect + ' out of ' + answers.length + '!';
+  elCorrect.textContent = 'You got ' + numCorrect + ' out of ' + answers.length + '!';
+};
