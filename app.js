94c34edfbc2eb104afb741ecf26fdec4eeeb942d
Kate Lucas <katherinelucas@gmail.com>
2016-01-17 12:10:23 -0800

    Added question 4

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var numCorrect = 0;
 var ans1 = document.getElementById('a1');
 var ans2 = document.getElementById('a2');
 var ans3 = document.getElementById('a3');
+var ans4 = document.getElementById('a4');
 var correctCount = document.getElementById('elCorrect');
 
-var questions = ['Can you guess what country I was born in?', 'Another fact about me is that I love sharks. Can you guess which shark species live in the Puget Sound? Choose one: great white shark, mako shark, sixgill shark or the black tip reef shark.', 'Can you guess how many siblings I have?'];
-var answers = ['PHILIPPINES', 'SIXGILL SHARK', 2];
-var els = [ans1, ans2, ans3];
+var questions = ['Can you guess what country I was born in?',
+                 'Another fact about me is that I love sharks. Can you guess which shark species live in the Puget Sound? Choose one: great white shark, mako shark, sixgill shark or the black tip reef shark.',
+                 'Can you guess how many siblings I have?',
+                 'I\'m thinking of a number between 1 and 10. Can you guess it?'];
+var answers = ['PHILIPPINES', 'SIXGILL SHARK', 2, 8];
+var els = [ans1, ans2, ans3, ans4];
 
 //Game function should take from questions array compares user answers to answers array and posts to html
 function game(questions, answers, index) {
@@ -26,8 +30,9 @@ if (queary === answers || parseInt(queary) === answers) {
 }
 
 for (var i = 0; i < questions.length; i++) {
-  window.setTimeout(game(questions[i], answers[i], i), 1000);
+  game(questions[i], answers[i], i);
 }
 
 elCorrect.textContent = 'You got ' + numCorrect + ' out of ' + answers.length + '!';
+
 };
